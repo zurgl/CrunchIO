a05c5a4cc1cb169a8e43eafbedb2657b880122c9
zurgl <zurgly@gmail.com>
2023-09-29 00:57:46 +0200

    some cleanup

--- tests/startup-scripts.rs
+++ tests/startup-scripts.rs
@@ -1,7 +1,7 @@
 use crunchio::CrunchIO;
 
 #[test]
-fn test_get_all_startup_scripts() {
+fn test_get_startup_scripts() {
     let client = CrunchIO::default();
 
     let name = "new_startup";
@@ -38,44 +38,3 @@ apt upgrade -y
     let deleted = client.delete_startup_script_by_id(&startup_script_id);
     assert_eq!(deleted.count, 1);
 }
-
-// #[test]
-// fn test_delete_startup_scripts() {
-//     let client = CrunchIO::default();
-//     let _ = client.delete_startup_scripts();
-// }
-
-// #[test]
-// fn test_get_all_startup_scripts() {
-//     let client = CrunchIO::default();
-//     let scripts = client.get_all_startup_scripts();
-//     println!("{scripts:#?}")
-// }
-
-// #[test]
-// fn test_add_startup_script() {
-//     let client = CrunchIO::default();
-//     let name = "new_startup";
-//     let script = r"
-//       #!/bin/bash
-//       apt update
-//       apt upgrade -y
-//       apt -y install tree unzip zip plocate docker-compose\
-//         libssl-dev ffmpeg imagemagick nvtop zlib1g \
-//         ninja-build python3-pip direnv git-crypt
-//       useradd -m -p $(openssl passwd -1 ${PASSWORD}) -s /bin/bash -G sudo ${USERNAME}
-//     ";
-//     let _ = client.add_startup_script(name, script);
-// }
-
-// #[test]
-// fn test_get_startup_script_by_id() {
-//     let client = CrunchIO::default();
-//     let _ = client.get_startup_script_by_id();
-// }
-
-// #[test]
-// fn test_delete_startup_script_by_id() {
-//     let client = CrunchIO::default();
-//     let _ = client.delete_startup_script_by_id();
-// }
